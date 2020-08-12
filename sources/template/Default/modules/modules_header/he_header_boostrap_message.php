339166e6b6a345107ce192c78044419b3c146cc8
ClicShopping <Yae8mr2o*>
2020-08-12 10:42:56 +0200

    little improvment

--- sources/template/Default/modules/modules_header/he_header_boostrap_message.php
+++ sources/template/Default/modules/modules_header/he_header_boostrap_message.php
@@ -26,7 +26,7 @@
       $this->title = CLICSHOPPING::getDef('module_header_boostrap_message_title');
       $this->description = CLICSHOPPING::getDef('module_header_boostrap_message_description');
 
-      if ( defined('MODULE_HEADER_BOOSTRAP_MESSAGE_STATUS') ) {
+      if (defined('MODULE_HEADER_BOOSTRAP_MESSAGE_STATUS')) {
         $this->sort_order = MODULE_HEADER_BOOSTRAP_MESSAGE_SORT_ORDER;
         $this->enabled = (MODULE_HEADER_BOOSTRAP_MESSAGE_STATUS == 'True');
       }
